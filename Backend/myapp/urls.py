e677aa95df87da5d3a1f64504317a2f951c54e31
Guilherme <oguilhermeDeveloper@gmail.com>
2026-04-05 17:16:28 -0300

    Página de Noticias
    Página de noticias parcialmente feita.

--- Backend/myapp/urls.py
+++ Backend/myapp/urls.py
@@ -3,5 +3,10 @@ from myapp import views
 
 #urls do site vem aqui
 urlpatterns = [
-    path('', views.newsPage, name='newsPage'), 
+    path('', views.main, name='mainPage'), 
+    path('/Noticias', views.news, name='newsPage'),
+    path('/Resultados', views.results, name='resultsPage'),
+    path('/Pilotos', views.drivers, name='driversPage'),
+    path('/Equipes', views.teams, name='teamsPage'),
+    path('/Pistas', views.tracks, name='tracksPage'),
 ]
--- Backend/myapp/views.py
+++ Backend/myapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 #crie suas views aqui
-def newsPage(request):
+def main(request):
+    return render(request, 'main.html')
+
+def news(request):
     return render(request, 'news.html')
+
+def results(request):
+    return render(request, 'results.html')
+
+def drivers(request):
+    return render(request, 'drivers.html')
+
+def teams(request):
+    return render(request, 'teams.html')
+
+def tracks(request):
+    return render(request, 'tracks.html')
