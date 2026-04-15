370a8a4529e403b12136d44b8ddba1aa472d6860
Everton-Notreve <evertonedivaldo5@gmail.com>
2026-04-14 23:11:59 -0300

    tabela V3

--- Backend/myapp/models.py
+++ Backend/myapp/models.py
@@ -1,6 +1,32 @@
 from django.db import models
 from django.db.models import Sum 
 
+class Equipe(models.Model):
+    nomeE = models.CharField(max_length=100)
+    paisE = models.CharField(max_length=100)
+    vitorias = models.IntegerField(default=0)
+    best_time = models.CharField(max_length=100, blank=True, null=True)
+    bestP = models.CharField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return self.nomeE  # Mostra o nome da equipe no admin
+    
+    def atualizar_stats(self):
+        # Pega todos os pilotos desta equipe
+        pilotos = self.piloto_set.all()
+        
+        # Só calcula se tiver pelo menos um piloto
+        if pilotos.exists():
+            # Soma todos os campeao dos pilotos
+            self.vitorias = pilotos.aggregate(Sum('campeao'))['campeao__sum']
+            
+            melhor = pilotos.order_by('bestT').first()# Pega o piloto com menor bestT (melhor tempo)
+            self.best_time = f"{melhor.nome} - {melhor.bestT}s"
+            
+            campeao = pilotos.order_by('-campeao').first() #Pega o piloto com maior vitorias(campeao)
+            self.bestP = f"{campeao.nome} - {campeao.campeao} títulos" # e vai aparecer o nome do pilot com o tempo
+            self.save()# Salva as alterações no banco
+
 #Tabela de Pilotos:
 class Piloto(models.Model):
     nome= models.CharField(max_length=100)
@@ -17,3 +43,18 @@ class Piloto(models.Model):
         verbose_name = 'Piloto'
         verbose_name_plural = 'Pilotos'
         ordering = ['id']
+
+class Pista(models.Model):
+    nomeP= models.CharField(max_length=100)
+    paísP= models.CharField(max_length=100)
+    vencedores= models.CharField(max_length=100)
+
+#Tabela de Campeõs:
+class Campeoes(models.Model):
+    nome = models.CharField(max_length=100)
+    pais = models.CharField(max_length=100)
+    vitorias = models.IntegerField(default=0)
+    menor_tempo = models.DecimalField(max_digits=6, decimal_places=3)
+    
+    def __str__(self):
+        return self.nome
